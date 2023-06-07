ca9bf0698c93da2e08e644a3c75b35e88add1d29
divinejoshua <divineuche399@gmail.com>
2023-06-07 12:26:45 +0100

    started styling for camera canva

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     // Main box 
+    <main className="App mx-auto">
+
+      {/* Navbar */}
+      <nav className="nav-header border-b font-bold ">   
+        <div className='pb-4 pl-5 pr-5 logo-text font-bold'>
+          Wearkit VR app   
+          {/* Link to twitter account  */}
+          <a href='https://twitter.com/Divine_Er' rel="noreferrer" target="_blank" > 
+            <span className='float-right'> <i className="fa fa-brands fa-twitter text-gray-400"></i></span>
+          </a>
+          {/* Link to codebase  */}
+          <a href='https://github.com/divinejoshua/ten-wonders-react-js' rel="noreferrer" target="_blank" >
+            <span className='float-right mr-4'> <i className="fa fa-brands fa-github text-gray-400"></i></span>
+          </a>
+        </div>
+      </nav>
+
+      {/* Camera canva  */}
+      <section className='max-w-7xl'>
+        <div className='camera-canva'></div>
+      </section>
+
+    </main>
   );
 }
 
