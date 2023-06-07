63c5ebf0fd888c36ac892786d10b023d5aa3139a
divinejoshua <divineuche399@gmail.com>
2023-06-07 19:16:53 +0100

    Added list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // States 
+  const [accessoryList, setaccessoryList] = useState([
+      { name: 'Item 1', id: 1 },
+      { name: 'Item 2', id: 2 },
+      { name: 'Item 3', id: 3 },
+      { name: 'Item 4', id: 4 },
+      { name: 'Item 5', id: 5 },
+      { name: 'Item 6', id: 6 },
+      { name: 'Item 7', id: 7 },
+  ]);
+
+
+
+
+
   return (
      // Main box 
     <main className="App mx-auto">
 
       {/* Navbar */}
-      <nav className="nav-header border-b font-bold ">   
-        <div className='pb-4 pl-5 pr-5 logo-text font-bold'>
+      <nav className="nav-header font-bold ">   
+        <div className='p-3 logo-text font-bold'>
           Wearkit VR app   
           {/* Link to twitter account  */}
           <a href='https://twitter.com/Divine_Er' rel="noreferrer" target="_blank" > 
@@ -22,8 +38,30 @@ function App() {
       </nav>
 
       {/* Camera canva  */}
-      <section className='max-w-7xl'>
-        <div className='camera-canva'></div>
+      <section className='mt-20 p-3'>
+          <div className='camera-canva bg-gray-200 rounded'></div>
+      </section>
+
+      <section className='p-3'>
+        <p className='text-gray-600'>Virtual accessories</p>
+
+        <div className="accessory-list p-2 mt-3">
+        {accessoryList.map((item, index) => (
+          <div className="accessory-item h-20 w-20 mr-3 border-4 cursor-pointer border-blue-500 rounded-xl">
+          </div>
+          ))}
+
+        </div>
+
+
+      </section>
+
+      <section className="about p-3 mb-10">
+      <h5 className='font-bold text-gray-400 mb-2'>About</h5>
+        <p>Wearkit VR uses machine learning and augamented reality to create a virtual experience for users to put on virtual accessories</p>
+
+        <h5 className='font-bold text-gray-400 mt-3 mb-2'>Use case</h5>
+        <p><span className="underline text-blue-700">An online shop</span> can make use of virtual reality (VR) in several ways to enhance the shopping experience for customers.</p>
       </section>
 
     </main>
