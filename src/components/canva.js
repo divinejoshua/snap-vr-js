a9386f768ed8314aa115f07585e4ddbba6f8ba77
divinejoshua <divineuche399@gmail.com>
2023-06-09 13:18:02 +0100

    Completed wear kit

--- src/components/canva.js
+++ src/components/canva.js
@@ -5,6 +5,13 @@ import ImageHat1 from '../assets/wears/hat-1.png';
 import ImageHat2 from '../assets/wears/hat-2.png';
 import ImageHat3 from '../assets/wears/hat-3.png';
 
+import ImageGlass1 from '../assets/wears/glass-1.png';
+import ImageGlass2 from '../assets/wears/glass-2.png';
+import ImageGlass3 from '../assets/wears/glass-3.png';
+import ImageGlass4 from '../assets/wears/glass-4.png';
+import ImageGlass5 from '../assets/wears/glass-5.png';
+import ImageGlass6 from '../assets/wears/glass-6.png';
+
 
 export default function Canva({itemSelected}) {
 
@@ -17,10 +24,21 @@ export default function Canva({itemSelected}) {
 
         const videoElement = videoRef.current;
         const sketchElement = sketchRef.current;
+        let itemNumberRefN =itemNumberRef.current;
+
+        // Hat Images 
         let ImageHat1Element =ImageRef.current;
         let ImageHat2Element =ImageRef.current;
         let ImageHat3Element =ImageRef.current;
-        let itemNumberRefN =itemNumberRef.current;
+        
+        // Glasses Images 
+        let ImageGlass1Element =ImageRef.current;
+        let ImageGlass2Element =ImageRef.current;
+        let ImageGlass3Element =ImageRef.current;
+        let ImageGlass4Element =ImageRef.current;
+        let ImageGlass5Element =ImageRef.current;
+        let ImageGlass6Element =ImageRef.current;
+        
         
 
         // videoElement.style.display = 'none';
@@ -43,6 +61,12 @@ export default function Canva({itemSelected}) {
                 ImageHat1Element = context.loadImage(ImageHat1);
                 ImageHat2Element = context.loadImage(ImageHat2);
                 ImageHat3Element = context.loadImage(ImageHat3);
+                ImageGlass1Element = context.loadImage(ImageGlass1);
+                ImageGlass2Element = context.loadImage(ImageGlass2);
+                ImageGlass3Element = context.loadImage(ImageGlass3);
+                ImageGlass4Element = context.loadImage(ImageGlass4);
+                ImageGlass5Element = context.loadImage(ImageGlass5);
+                ImageGlass6Element = context.loadImage(ImageGlass6);
               };
     
             context.setup = () => {
@@ -94,6 +118,28 @@ export default function Canva({itemSelected}) {
                 if(Number(itemNumberRefN.textContent)===6) {
                     context.image(ImageHat3Element, ((leftEyeX + rightEyeX) / 2)-110, ((leftEyeY + rightEyeY) / 2)-200, 220, 220);
                 }
+
+                // Glasses Items s
+                if(Number(itemNumberRefN.textContent)===1) {
+                    context.image(ImageGlass1Element, ((leftEyeX + rightEyeX) / 2)-110, ((leftEyeY + rightEyeY) / 2)-100, 220, 220);
+                }
+                if(Number(itemNumberRefN.textContent)===3) {
+                    context.image(ImageGlass2Element, ((leftEyeX + rightEyeX) / 2)-110, ((leftEyeY + rightEyeY) / 2)-100, 220, 220);
+                }
+                if(Number(itemNumberRefN.textContent)===5) {
+                    context.image(ImageGlass3Element, ((leftEyeX + rightEyeX) / 2)-110, ((leftEyeY + rightEyeY) / 2)-100, 220, 220);
+                }
+                if(Number(itemNumberRefN.textContent)===7) {
+                    context.image(ImageGlass4Element, ((leftEyeX + rightEyeX) / 2)-110, ((leftEyeY + rightEyeY) / 2)-100, 220, 220);
+                }
+                if(Number(itemNumberRefN.textContent)===8) {
+                    context.image(ImageGlass5Element, ((leftEyeX + rightEyeX) / 2)-110, ((leftEyeY + rightEyeY) / 2)-100, 220, 220);
+                }
+                if(Number(itemNumberRefN.textContent)===9) {
+                    context.image(ImageGlass6Element, ((leftEyeX + rightEyeX) / 2)-110, ((leftEyeY + rightEyeY) / 2)-100, 220, 220);
+                }
+
+                
             };
     
         };
